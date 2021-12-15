44469f100fa91f682ccada9671a2895588118e2a
Markvitim <markvitim@gmail.com>
2021-12-15 18:13:55 +0300

    all test added

--- src/main/java/ru/netology/statService/StatService.java
+++ src/main/java/ru/netology/statService/StatService.java
@@ -1,11 +1,59 @@
 package ru.netology.statService;
 
 public class StatService {
-    public int calculateSumSales(int[] sales){
+    public int calculateSumSales(int[] sales) {
         int sum = 0;
-        for(int sale:sales){
-            sum+=sale;
+        for (int sale : sales) {
+            sum += sale;
         }
         return sum;
+    }
+
+    public int calcAverSalePerMonth(int[] sales) {
+        return calculateSumSales(sales) / sales.length;
+    }
+
+    public int maxMonth(int[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month += 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minMonth(int[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month += 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int numMonthLowAverSale(int[] sales) {
+        int counter = 0;
+        for (int sale : sales) {
+            if (sale < calcAverSalePerMonth(sales)) {
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+
+    public int numMonthUpperAverSale(int[] sales) {
+        int counter = 0;
+        for (int sale : sales) {
+            if (sale > calcAverSalePerMonth(sales)) {
+                counter += 1;
+            }
+        }
+        return counter;
     }
 }
--- src/test/java/ru/netology/statService/StatServiceTest.java
+++ src/test/java/ru/netology/statService/StatServiceTest.java
@@ -18,5 +18,74 @@ class StatServiceTest {
 
     }
 
+    @org.junit.jupiter.api.Test
+    void calcAverSalePerMonth() {
+
+        StatService service = new StatService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.calcAverSalePerMonth(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void maxMonth() {
+
+        StatService service = new StatService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        int actual = service.maxMonth(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void minMonth() {
+
+        StatService service = new StatService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.minMonth(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void numMonthLowAverSale() {
+
+        StatService service = new StatService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.numMonthLowAverSale(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void numMonthUpperAverSale() {
+
+        StatService service = new StatService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.numMonthUpperAverSale(sales);
+
+        assertEquals(expected, actual);
+
+    }
 }
 
